2d705138ca9040bf705ef768163e0b051b3c37fa
Jia Liang <hojialiang@protonmail.com>
2025-04-17 09:24:02 +0800

    Fix error encoding if space in filename

--- index.bat
+++ index.bat
@@ -27,27 +27,27 @@ goto end
 
 :avif
 
-avifenc -q %Quality% -s 0 --sharpyuv %Name%.png %Name%.avif > nul
-avifdec %Name%.avif output.png > nul
+avifenc -q %Quality% -s 0 --sharpyuv "%Name%.png" "%Name%.avif" > nul
+avifdec "%Name%.avif" output.png > nul
 goto compare
 
 :webp
 
-cwebp -q %Quality% -m 6 -pass 10 -mt -sharp_yuv %Name%.png -o %Name%.webp > nul
-dwebp %Name%.webp -o output.png > nul
+cwebp -q %Quality% -m 6 -pass 10 -mt -sharp_yuv "%Name%.png" -o "%Name%.webp" > nul
+dwebp "%Name%.webp" -o output.png > nul
 goto compare
 
 :jxl
 
-cjxl -q %Quality% -e 10 %Name%.png %Name%.jxl > nul
-djxl %Name%.jxl output.png > nul
+cjxl -q %Quality% -e 10 "%Name%.png" "%Name%.jxl" > nul
+djxl "%Name%.jxl" output.png > nul
 goto compare
 
 :compare
 
 echo.
 echo Score for %Format% with quality %Quality%:
-"./ssimulacra2/ssimulacra2.exe" %Name%.png output.png
+"./ssimulacra2/ssimulacra2.exe" "%Name%.png" output.png
 del output.png
 goto end
 
