97da4db53eab1196047113d60127c7517f4caca0
Jia Liang <hojialiang@protonmail.com>
2025-04-26 16:36:22 +0800

    Better variable names

--- iot.bat
+++ iot.bat
@@ -1,11 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set Name=
-set Extension=
-set Format=%~1
+set InName=
+set InType=
+set OutType=%~1
+
 set Quality=%~2
 set Lossless=
+
 set Size=
 set Score=
 
@@ -26,21 +28,21 @@ if "%~2" == "" (
 if "%~2" == "lossless" (
   set Quality=100
   
-  if "%Format%" == "avif" ( set Lossless="--lossless" )
-  if "%Format%" == "webp" ( set Lossless="-lossless" )
+  if "%OutType%" == "avif" ( set Lossless="--lossless" )
+  if "%OutType%" == "webp" ( set Lossless="-lossless" )
 )
 
 if not "%~3" == "" (
   if exist "%~3" (
-    for %%F in ("%~3") do set Name=%%~nF
+    for %%F in ("%~3") do set InName=%%~nF
     
     if /I "%~x3" == ".png" (
-      set Extension=png
+      set InType=png
       goto found
     )
     
     if /I "%~x3" == ".jpg" (
-      set Extension=jpg
+      set InType=jpg
       goto found
     )
     
@@ -53,25 +55,25 @@ if not "%~3" == "" (
 )
 
 for %%F in (*.png) do (
-  set Name=%%~nF
-  set Extension=png
+  set InName=%%~nF
+  set InType=png
   goto found
 )
 
 for %%F in (*.jpg) do (
-  set Name=%%~nF
-  set Extension=jpg
+  set InName=%%~nF
+  set InType=jpg
   goto found
 )
 
-if not defined Name (
+if not defined InName (
   echo Error: No PNG/JPG image found in current directory.
   goto end
 )
 
 :found
 
-echo "%Name%.%Extension%" found, coverting to %Format%.
+echo "%InName%.%InType%" found, coverting to %OutType%.
 
 if not exist "history" ( mkdir "history" )
 
@@ -80,69 +82,69 @@ if not exist "history" (
   goto end
 )
 
-del "%Name%.%Format%" > nul 2>&1
-del "%Name%.%Format%.png" > nul 2>&1
+del "%InName%.%OutType%" > nul 2>&1
+del "%InName%.%OutType%.png" > nul 2>&1
 
-if "%Format%" == "avif" (
-  avifenc -q %Quality% %Lossless% -s 0 --sharpyuv "%Name%.%Extension%" "%Name%.avif" > nul 2>&1
+if "%OutType%" == "avif" (
+  avifenc -q %Quality% %Lossless% -s 0 --sharpyuv "%InName%.%InType%" "%InName%.avif" > nul 2>&1
 )
 
-if "%Format%" == "webp" (
-  cwebp -q %Quality% %Lossless% -m 6 -pass 10 -mt -sharp_yuv "%Name%.%Extension%" -o "%Name%.webp" > nul 2>&1
+if "%OutType%" == "webp" (
+  cwebp -q %Quality% %Lossless% -m 6 -pass 10 -mt -sharp_yuv "%InName%.%InType%" -o "%InName%.webp" > nul 2>&1
 )
 
-if "%Format%" == "jxl" (
-  cjxl -q %Quality% -e 10 "%Name%.%Extension%" "%Name%.jxl" > nul 2>&1
+if "%OutType%" == "jxl" (
+  cjxl -q %Quality% -e 10 "%InName%.%InType%" "%InName%.jxl" > nul 2>&1
 )
 
-if not exist "%Name%.%Format%" (
-  echo Error: Failed to create "%Name%.%Format%".
+if not exist "%InName%.%OutType%" (
+  echo Error: Failed to create "%InName%.%OutType%".
   goto end
 )
 
-if "%Format%" == "avif" (
-  avifdec "%Name%.%Format%" "%Name%.%Format%.png" > nul 2>&1
+if "%OutType%" == "avif" (
+  avifdec "%InName%.%OutType%" "%InName%.%OutType%.png" > nul 2>&1
 )
 
-if "%Format%" == "webp" (
-  dwebp "%Name%.%Format%" -o "%Name%.%Format%.png" > nul 2>&1
+if "%OutType%" == "webp" (
+  dwebp "%InName%.%OutType%" -o "%InName%.%OutType%.png" > nul 2>&1
 )
 
-if "%Format%" == "jxl" (
-  djxl "%Name%.%Format%" "%Name%.%Format%.png" > nul 2>&1
+if "%OutType%" == "jxl" (
+  djxl "%InName%.%OutType%" "%InName%.%OutType%.png" > nul 2>&1
 )
 
-if not exist "%Name%.%Format%.png" (
-  echo Error: Failed to create "%Name%.%Format%.png".
+if not exist "%InName%.%OutType%.png" (
+  echo Error: Failed to create "%InName%.%OutType%.png".
   goto end
 )
 
-for /f %%i in ('"ssimulacra2 "%Name%.%Extension%" "%Name%.%Format%.png""') do (
+for /f %%i in ('ssimulacra2 "%InName%.%InType%" "%InName%.%OutType%.png"') do (
   set Score=%%i
 )
 
-del "%Name%.%Format%.png" > nul
+del "%InName%.%OutType%.png" > nul
 
 if not defined Score (
-  echo Error: Failed to generate score for "%Name%.%Format%".
+  echo Error: Failed to generate score for "%InName%.%OutType%".
   goto end
 )
 
 set "Score=!Score:~0,6!"
 
-for %%I in ("%Name%.%Format%") do set "Size=%%~zI"
+for %%I in ("%InName%.%OutType%") do set "Size=%%~zI"
 
 set /a Size=(Size + 1023) / 1024
 
 if %Size% LSS 1 set Size=1
 
-copy "%Name%.%Format%" "./history/%Name%_(Q=%~2)(S=%Score%).%Format%" /Y > nul 2>&1
+copy "%InName%.%OutType%" "./history/%InName%_(Q=%~2)(S=%Score%).%OutType%" /Y > nul 2>&1
 
-if not exist "./history/%Name%_(Q=%~2)(S=%Score%).%Format%" (
+if not exist "./history/%InName%_(Q=%~2)(S=%Score%).%OutType%" (
   echo "Warning: Unable to write to history folder."
 )
 
-echo "%Name%.%Format%" with quality %~2 and size %Size%KB has score %Score%.
+echo "%InName%.%OutType%" with quality %~2 and size %Size%KB has score %Score%.
 echo.
 
 :end
