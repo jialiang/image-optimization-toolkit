856783710a405a82786df052a31ea48c101578c2
Jia Liang <hojialiang@protonmail.com>
2025-04-26 18:49:53 +0800

    Make code format more consistent

--- iot.bat
+++ iot.bat
@@ -3,77 +3,72 @@ setlocal enabledelayedexpansion
 
 set InName=
 set InType=
-set OutType=%~1
+set OutType=
 
-set Quality=%~2
+set Quality=
 set Lossless=
 
 set Size=
 set Score=
 
-set PATH=%PATH%;%~dp0ssimulacra2;%~dp0encoders;
+set "PATH=%PATH%;%~dp0ssimulacra2;%~dp0encoders";
 
 if "%~1" == "" (
-  echo Argument 1 needs to be keyword avif, webp or jxl.
-  echo Argument 2 needs to be keyword lossless or an integer from 0 to 100.
-  echo Argument 3 can be the filename of a PNG/JPG image, otherwise the first PNG/JPG image found in current directory is used.
+  echo Argument 1 needs to be keyword "avif", "webp" or "jxl".
+  echo Argument 2 needs to be keyword "lossless" or an integer from 0 to 100.
+  echo Argument 3 can be the filename of a PNG/JPG image, otherwise the first PNG/JPG image in the current directory is used.
   goto end
 )
+
+set "OutType=%~1"
 
 if "%~2" == "" (
   echo Error: Argument 2 needs to be keyword "lossless" or an integer from 0 to 100.
   goto end
 )
 
+set "Quality=%~2"
+
 if "%~2" == "lossless" (
-  set Quality=100
+  set "Quality=100"
   
-  if "%OutType%" == "avif" ( set Lossless="--lossless" )
-  if "%OutType%" == "webp" ( set Lossless="-lossless" )
+  if "%OutType%" == "avif" ( set "Lossless=--lossless" )
+  if "%OutType%" == "webp" ( set "Lossless=-lossless" )
 )
 
 if not "%~3" == "" (
-  if exist "%~3" (
-    for %%F in ("%~3") do set InName=%%~nF
-    
-    if /I "%~x3" == ".png" (
-      set InType=png
-      goto found
-    )
-    
-    if /I "%~x3" == ".jpg" (
-      set InType=jpg
-      goto found
-    )
-    
-    echo Error: "%~3" does not have the extension .png or .jpg.
+  if not exist "%~3" (
+    echo Error: "%~3" not found.
     goto end
   )
   
-  echo Error: "%~3" not found.
+  for %%F in ("%~3") do (
+    set "InName=%%~nF"
+    set "InType=%%~xF"
+  )
+  
+  if /I "!InType!" == ".png" ( goto found )
+  if /I "!InType!" == ".jpg" ( goto found )
+  
+  echo Error: "%~3" does not have the extension .png or .jpg.
   goto end
 )
 
-for %%F in (*.png) do (
-  set InName=%%~nF
-  set InType=png
+for %%F in (*.png *.jpg) do (
+  set "InName=%%~nF"
+  set "InType=%%~xF"
   goto found
 )
 
-for %%F in (*.jpg) do (
-  set InName=%%~nF
-  set InType=jpg
-  goto found
-)
-
-if not defined InName (
-  echo Error: No PNG/JPG image found in current directory.
-  goto end
-)
+echo Error: No PNG/JPG image found in current directory.
+goto end
 
 :found
 
-echo "%InName%.%InType%" found, coverting to %OutType%.
+set "Input=%InName%%InType%"
+set "Output=%InName%.%OutType%"
+
+echo "%Input%" found, coverting to %OutType%.
 
 if not exist "history" ( mkdir "history" )
 
@@ -82,57 +77,46 @@ if not exist "history" (
   goto end
 )
 
-del "%InName%.%OutType%" > nul 2>&1
-del "%InName%.%OutType%.png" > nul 2>&1
+del "%Output%" > nul 2>&1
+del "%Output%.png" > nul 2>&1
 
 if "%OutType%" == "avif" (
-  avifenc -q %Quality% %Lossless% -s 0 --sharpyuv "%InName%.%InType%" "%InName%.avif" > nul 2>&1
+  avifenc -q %Quality% %Lossless% -s 0 --sharpyuv "%Input%" "%Output%" > nul 2>&1
+  avifdec "%Output%" "%InName%.%OutType%.png" > nul 2>&1
 )
 
 if "%OutType%" == "webp" (
-  cwebp -q %Quality% %Lossless% -m 6 -pass 10 -mt -sharp_yuv "%InName%.%InType%" -o "%InName%.webp" > nul 2>&1
+  cwebp -q %Quality% %Lossless% -m 6 -pass 10 -mt -sharp_yuv "%Input%" -o "%Output%" > nul 2>&1
+  dwebp "%Output%" -o "%Output%.png" > nul 2>&1
 )
 
 if "%OutType%" == "jxl" (
-  cjxl -q %Quality% -e 10 "%InName%.%InType%" "%InName%.jxl" > nul 2>&1
+  cjxl -q %Quality% -e 10 "%Input%" "%Output%" > nul 2>&1
+  djxl "%Output%" "%Output%.png" > nul 2>&1
 )
 
-if not exist "%InName%.%OutType%" (
-  echo Error: Failed to create "%InName%.%OutType%".
+if not exist "%Output%" (
+  echo Error: Failed to create "%Output%".
   goto end
 )
 
-if "%OutType%" == "avif" (
-  avifdec "%InName%.%OutType%" "%InName%.%OutType%.png" > nul 2>&1
-)
-
-if "%OutType%" == "webp" (
-  dwebp "%InName%.%OutType%" -o "%InName%.%OutType%.png" > nul 2>&1
-)
-
-if "%OutType%" == "jxl" (
-  djxl "%InName%.%OutType%" "%InName%.%OutType%.png" > nul 2>&1
-)
-
-if not exist "%InName%.%OutType%.png" (
-  echo Error: Failed to create "%InName%.%OutType%.png".
+if not exist "%Output%.png" (
+  echo Error: Failed to create "%Output%.png".
   goto end
 )
 
-for /f %%i in ('ssimulacra2 "%InName%.%InType%" "%InName%.%OutType%.png"') do (
-  set Score=%%i
-)
+for /f %%F in ('ssimulacra2 "%Input%" "%Output%.png"') do ( set "Score=%%F" )
 
-del "%InName%.%OutType%.png" > nul
+del "%Output%.png" > nul
 
 if not defined Score (
-  echo Error: Failed to generate score for "%InName%.%OutType%".
+  echo Error: Failed to generate score for "%Output%".
   goto end
 )
 
 set "Score=!Score:~0,6!"
 
-for %%I in ("%InName%.%OutType%") do set "Size=%%~zI"
+for %%F in ("%Output%") do ( set "Size=%%~zF" )
 
 set /a Size=(Size + 1023) / 1024
 
@@ -144,7 +128,7 @@ if not exist "./history/%InName%_(Q=%~2)(S=%Score%).%OutType%" (
   echo "Warning: Unable to write to history folder."
 )
 
-echo "%InName%.%OutType%" with quality %~2 and size %Size%KB has score %Score%.
+echo "%Output%" with quality %~2 and size %Size%KB has score %Score%.
 echo.
 
 :end
